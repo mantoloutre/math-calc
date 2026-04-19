e7c709cc1f8456c8204d58479bc5253b1a166a71
mantoloutre <200654635+mantoloutre@users.noreply.github.com>
2026-04-19 02:11:20 -0400

    commencement minim page pour trouver point avec vecteur

--- src/app/PageTrouverPointAvecVecteur.java
+++ src/app/PageTrouverPointAvecVecteur.java
@@ -1,10 +1,15 @@
 package app;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * documentation a faire
  * 
  * @author mantoloutre
  */
-public class PageTrouverPointAvecVecteur {
-
+public class PageTrouverPointAvecVecteur extends PageDeBase {
+    public PageTrouverPointAvecVecteur(CardLayout cl, JPanel conteneur) {
+        super(cl, conteneur);
+    }
 }
