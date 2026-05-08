4534aca94d0916a405be13cec5fe717f0c5eed27
mantoloutre <200654635+mantoloutre@users.noreply.github.com>
2026-05-08 18:00:10 -0400

    retirer de la documentation inutile sur les getters et les setters

--- src/app/models/Triangle.java
+++ src/app/models/Triangle.java
@@ -4,7 +4,7 @@ package app.models;
  * classe qui représente un objet de type triangle.
  * cette classe contient aussi divers méthode pour faire des calculs avec les
  * triangles
- * 
+ *
  * @author mantoloutre
  */
 public class Triangle {
@@ -12,56 +12,26 @@ public class Triangle {
     private double coterB;
     private double coterC;
 
-    /**
-     * getter pour la longueur de la base dans un triangle rectangle
-     * 
-     * @return retourne la longueur de la base
-     */
     public double getBase() {
         return this.coterA;
     }
 
-    /**
-     * getter pour la longueur de la hauteur dans un triangle rectangle
-     * 
-     * @return retourne la longueur de la hauteur
-     */
     public double getHauteur() {
         return this.coterB;
     }
 
-    /**
-     * getter pour la longueur de l'hypothénuse dans un triangle rectangle
-     * 
-     * @return retourne la longueur de l'hypothénuse
-     */
     public double getHypo() {
         return this.coterC;
     }
 
-    /**
-     * setter pour le coter A ( ou la base pour un triangle rectangle )
-     * 
-     * @param coterA la longueur du coter
-     */
     public void setCoterA(double coterA) {
         this.coterA = coterA;
     }
 
-    /**
-     * getter pour le coter A ( ou la base pour un triangle rectangle )
-     * 
-     * @return retourne la longueur du coter A
-     */
     public double getCoterA() {
         return this.coterA;
     }
 
-    /**
-     * setter pour le coter B ( ou la hauteur dans un triangle rectangle )
-     * 
-     * @param coterB la longueur de la hauteur
-     */
     public void setCoterB(double coterB) {
         this.coterB = coterB;
     }
@@ -70,11 +40,6 @@ public class Triangle {
         return this.coterB;
     }
 
-    /**
-     * méthode pour set le coter C ( ne s'applique par pour un triangle rectangle )
-     * 
-     * @param coterC la longueur du coter C
-     */
     public void setCoterC(double coterC) {
         this.coterC = coterC;
     }
@@ -85,7 +50,7 @@ public class Triangle {
 
     /**
      * constructeur pour faire un triangle a partir de trois points
-     * 
+     *
      * @param pA le premier point
      * @param pB le deuxième point
      * @param pC le troisième point
@@ -99,9 +64,9 @@ public class Triangle {
     /**
      * constructeur pour faire un triangle sachant que l'hypothénuse est calculer
      * automatiquement
-     * 
+     *
      * <h3>triangle rectangle uniquement!</h3>
-     * 
+     *
      * @param base    base du triangle
      * @param hauteur hauteur du triangle
      */
@@ -113,7 +78,7 @@ public class Triangle {
 
     /**
      * méthode pour calculer la différence de grosseur ( k ) avec deux triangles
-     * 
+     *
      * @param t1 le premier triangle
      * @param t2 le deuxième triangle
      * @return retourne le rapport de grosseur
@@ -141,7 +106,7 @@ public class Triangle {
 
     /**
      * méthode pour calculer l'aire d'un triangle
-     * 
+     *
      * @param t1 le triangle dont on veut calculer l'aire
      * @return ( base x hauteur ) / 2 qui représente l'aire
      */
@@ -152,12 +117,12 @@ public class Triangle {
 
     /**
      * méthode pour calculer la longueur de chacun des côter d'un triangle
-     * 
+     *
      * @param pA le premier point du triangle
      * @param pB le deuxième point du triangle
      * @param pC le troisième point du triangle
      * @return retourne un tableau contenant respectivement : la longueur du côter :
-     *         A, B et C
+     * A, B et C
      */
     public static double[] calculLongueur(Point pA, Point pB, Point pC) {
         // calculer la longueur de chacun des côté //
@@ -165,6 +130,6 @@ public class Triangle {
         longA = Vecteur.calculerNorme(pA, pB);
         longB = Vecteur.calculerNorme(pB, pC);
         longC = Vecteur.calculerNorme(pC, pA);
-        return new double[]{ longA, longB, longC };
+        return new double[]{longA, longB, longC};
     }
 }
