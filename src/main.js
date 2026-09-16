21f469a6524321ebff815aec04ecc2fc060d27e5
mantoloutre <200654635+mantoloutre@users.noreply.github.com>
2026-09-15 21:14:40 -0400

    feat(js): bind submit to tauri invoke

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
-const { invoke } = window.__TAURI__.core;
+const invoke = window.__TAURI__.core.invoke;
 
-let greetInputEl;
-let greetMsgEl;
+const form = document.querySelector('#vector-form');
+const xInput = document.querySelector('#x-val');
+const yInput = document.querySelector('#y-val');
+const outputEl = document.querySelector('#output');
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function handleCalculate(e) {
+    e.preventDefault();
+
+    const x = parseFloat(xInput.value);
+    const y = parseFloat(yInput.value);
+
+    outputEl.textContent = await invoke('magnitude', {x, y});
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+window.addEventListener('DOMContentLoaded', () => {
+    if (form) {
+        form.addEventListener('submit', handleCalculate);
+    }
 });
